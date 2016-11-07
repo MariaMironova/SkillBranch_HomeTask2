88162f9f945afe21897d975ab809e85d2ac8ddf3
Maria <masha.a.mironova@yandex.ru>
2016-11-07 18:45:15 +0300

    add fio

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import sum from './sum';
+import fio from './fio';
 
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
-  const a = req.query.a;
-  const b = req.query.b;
-  const summa = '' + sum(a, b);
-  res.send(summa);
+  const fullname = req.query.fullname;
+  res.send(fio(fullname));
 });
 
 app.listen(3000, () => {
--- src/sum.js
+++
@@ -1,3 +0,0 @@
-export default function sum(a = 0, b = 0) {
-  return +a + +b;
-}
