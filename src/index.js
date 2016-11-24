faffab1cf8dd102274dc0010cfc2c62820f052c7
Maria <masha.a.mironova@yandex.ru>
2016-11-24 16:24:23 +0300

    add rgb & hsl

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'isomorphic-fetch';
+import hsl from 'hsl-to-hex';
 
 const app = express();
 app.use(cors());
 
-function getColor(color) {
+function hsl2hex(hslColor) {
+  hslColor = hslColor.match(/^hsl\(\s*(\d+)\s*,\s*%20(\d+)%\s*,\s*%20(\d+)%\s*\)$/);
+  //if (!hslColor) return 'Invalid color';
+  if ( (!hslColor) || (hslColor[2] < 0) || hslColor[2] > 100 || hslColor[3] < 0 || hslColor[3] > 100) return 'Invalid color';
+  console.log(hslColor);
+  return hsl(hslColor[1], hslColor[2], hslColor[3]);
+  //color=hsl(195, 100%, 50%)
+}
+
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/);
+ if (!rgb) return 'Invalid color';
+ console.log(rgb);
+ var result = '#';
+ for (var i = 1; i <= 3; i++) {
+  var num = parseInt(rgb[i],10);
+  console.log(num);
+  if (num > 255 || num < 0) return 'Invalid color';
+  result += ("0" + num.toString(16)).slice(-2);
+ }
+ return result;
+}
+
+function hex2hex(color) {
   color = ((color.charAt(0) != '#') ? '#' : '') + color.toLowerCase();
   console.log(color);
   var isOk  = /(^#[0-9a-f]{6}$)|(^#[0-9a-f]{3}$)/i.test(color);
@@ -16,8 +40,25 @@ function getColor(color) {
   return isOk ? color : 'Invalid color';
 }
 
+function getColor(color) {
+  if (color.startsWith('rgb')) {
+    return rgb2hex(color);
+  }
+  if (color.startsWith('hsl')) return hsl2hex(color);
+  return hex2hex(color);
+  /*color = ((color.charAt(0) != '#') ? '#' : '') + color.toLowerCase();
+  console.log(color);
+  var isOk  = /(^#[0-9a-f]{6}$)|(^#[0-9a-f]{3}$)/i.test(color);
+  if (color.length == 4 && isOk) {
+    var newColor = color.substring(0,1) + color.charAt(1) + color.substring(1,3) + color.charAt(2) + color.charAt(3) + color.charAt(3);
+    color = newColor;
+  }*/
+  //return isOk ? color : 'Invalid color';
+}
+
 app.get('/', (req, res) => {
   const color = req.query.color;
+  console.log(color);
   if (color === undefined) {
     res.send('Invalid color');
   }
